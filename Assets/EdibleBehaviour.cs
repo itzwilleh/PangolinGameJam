83247b6de918b68ef204d2e935f53f165c43069e
Evan Mulrooney <evanmulrooney@gmail.com>
2018-04-28 11:06:32 -0400

    First commit

---
+++ Assets/EdibleBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EdibleBehaviour : MonoBehaviour {
+
+
+	public int pointValue;
+
+	public void Eat() {
+		Debug.Log("You ate: " + name + ", worth " + pointValue + " points");
+		Destroy(this.gameObject);
+	}
+
+}
---
+++ Assets/TongueBehaviour.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TongueBehaviour : MonoBehaviour {
+
+	public GameObject originArea;
+	public GameObject tonguePiece;
+	//public BoxCollider2D coll;
+
+	bool moving = false;
+
+	public float moveSpeed = 0.02f;
+	public float tonguePieceTimer = 0.5f;
+	string moveDir = "Down";
+
+	List<GameObject> tonguePieces = new List<GameObject>();
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Left")) {
+			moveDir = "Left";
+			Debug.Log("Left");
+		} else if (Input.GetButtonDown("Right")) {
+			moveDir = "Right";
+			Debug.Log("Right");
+		} else if (Input.GetButtonDown("Down")) {
+
+			if (!moving) {
+				moving = true;
+				InvokeRepeating("PlacePiece", tonguePieceTimer, tonguePieceTimer);
+			}
+
+			moveDir = "Down";
+			Debug.Log("Down");
+		}
+
+		if (moving) {
+			if (Input.GetButtonDown("Retract")) {
+				moveDir = "Down";
+				Retract();
+			}
+		}
+
+	}
+
+	void FixedUpdate() {
+		if (moving) {
+			if (moveDir == "Left") {
+				transform.position += new Vector3(-moveSpeed, 0, 0);
+			} else if (moveDir == "Right") {
+				transform.position += new Vector3(moveSpeed, 0, 0);
+			} else {
+				transform.position += new Vector3(0, -moveSpeed, 0);
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		Debug.Log("Collided with: " + coll.transform.name);
+
+		if (coll.GetComponent<EdibleBehaviour>() != null) {
+			coll.GetComponent<EdibleBehaviour>().Eat();
+		} else {
+			Retract();
+		}
+	}
+
+	private void Retract() {
+		moving = false;
+		CancelInvoke();
+		transform.position = originArea.transform.position;
+		tonguePieces = new List<GameObject>();
+
+	}
+
+	private void PlacePiece() {
+		GameObject newPiece = Instantiate(tonguePiece, transform.position, Quaternion.identity);
+		tonguePieces.Add(newPiece);
+	}
+
+}
